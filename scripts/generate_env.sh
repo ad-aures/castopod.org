2dbbb6d56e0620a0d23d7795ad8bcf7d0677c68f
Yassine Doghri <yassine@doghri.fr>
2022-02-20 13:42:45 +0100

    feat: add get started buttons redirecting to docs website
    - update gitlabci config
    - add .env file to reference castopod version and package url
    - add
    script to generate .env with latest version and package url

---
+++ scripts/generate_env.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Install jq to traverse json
+apt-get install jq -y -qq
+
+echo "Getting latest Castopod version and package urls ..."
+
+RELEASE_URL="https://code.castopod.org/api/v4/projects/2/releases"
+CASTOPOD_VERSION=$(echo $(curl --silent https://code.castopod.org/api/v4/projects/2/releases | jq '.[0] | .tag_name') | tr -d '"')
+LATEST_RELEASE_URL="https://code.castopod.org/api/v4/projects/2/releases/${CASTOPOD_VERSION}"
+CASTOPOD_PACKAGE_URL_1=$(echo $(curl --silent ${LATEST_RELEASE_URL} | jq '.assets.links[0].url') | tr -d '"')
+CASTOPOD_PACKAGE_URL_2=$(echo $(curl --silent ${LATEST_RELEASE_URL} | jq '.assets.links[1].url') | tr -d '"')
+
+echo "Done"
+
+echo "Creating .env file ..."
+
+echo PUBLIC_CASTOPOD_VERSION=${CASTOPOD_VERSION:1} > .env
+
+if [[ "$CASTOPOD_PACKAGE_URL_1" == *.zip ]]
+then
+    echo PUBLIC_CASTOPOD_PACKAGE_URL=$CASTOPOD_PACKAGE_URL_1 >> .env
+else
+    echo PUBLIC_CASTOPOD_PACKAGE_URL=$CASTOPOD_PACKAGE_URL_2 >> .env
+fi
+
+echo "Done"
---
+++ src/env.d.ts
@@ -0,0 +1,8 @@
+interface ImportMetaEnv {
+  readonly PUBLIC_CASTOPOD_PACKAGE_URL: string;
+  readonly PUBLIC_CASTOPOD_VERSION: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
