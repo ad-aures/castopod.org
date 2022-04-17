bf252337acbeee86b00c5794d8bde1001ea5c97c
Yassine Doghri <yassine@doghri.fr>
2022-04-17 11:47:44 +0000

    build: add devcontainer config with base extensions + add quality tools (eslint, stylelint, etc.)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap({
-    filter: true
-  }), partytown()],
+  integrations: [
+    tailwind(),
+    sitemap({
+      filter: true,
+    }),
+    partytown(),
+  ],
   vite: {
     ssr: {
-      external: ['svgo']
-    }
-  }
+      external: ["svgo"],
+    },
+  },
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -3,6 +3,7 @@ interface ImportMetaEnv {
   readonly PUBLIC_CASTOPOD_VERSION: string;
 }
 
+// eslint-disable-next-line no-unused-vars
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
