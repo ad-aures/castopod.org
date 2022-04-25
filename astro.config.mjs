0b6112a2d6744f715b3052e8f7c9c9c5766d6143
Yassine Doghri <yassine@doghri.fr>
2022-04-25 13:53:53 +0000

    fix: remove partytown integration for analytics to work properly + add AnalyticsEventScript to legal

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://castopod.org/",
-  integrations: [tailwind(), sitemap(), partytown()],
+  integrations: [tailwind(), sitemap()],
   vite: {
     ssr: {
       external: ["svgo"],
