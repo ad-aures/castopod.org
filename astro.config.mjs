68f5614b1265e54100769d236d8b523f45c9e843
Yassine Doghri <yassine@doghri.fr>
2022-05-12 14:31:54 +0000

    feat(i18n): use astro-i18next to translate website
    + add french translation strings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,26 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://castopod.org/",
-  integrations: [tailwind(), sitemap()],
+  experimental: {
+    integrations: true,
+  },
+  integrations: [
+    tailwind(),
+    sitemap(),
+    astroI18next({
+      resourcesPath: "./src/locales/",
+      i18nextConfig: {
+        fallbackLng: ["en", "fr"],
+        supportedLngs: ["en", "fr"],
+        debug: true,
+      },
+    }),
+  ],
   vite: {
     ssr: {
       external: ["svgo"],
